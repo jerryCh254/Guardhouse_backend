a3f51fca610bd10bbd26a88a123b125d996f0c8a
jerryCh254 <jerrych254@gmail.com>
2026-01-30 13:50:18 +0500

    push

--- Routes/site/siteRoutes.js
+++ Routes/site/siteRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createSite } = require('../../Controllers/customer/siteControllers');
+const { createSite } = require('../../Controllers/customer/customerSiteControllers');
 
 router.post('/createSite',createSite);
 module.exports = router;
