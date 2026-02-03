4d33ad36b98e44a1b13c35a86b3f888cf44543d6
jerryCh254 <jerrych254@gmail.com>
2026-02-03 15:13:21 +0500

    kkkk

--- Controllers/sites/siteDocumentController.js
+++ Controllers/sites/siteDocumentController.js
@@ -1,56 +1,52 @@
-const SiteDocument = require('../../models/site/siteDocumentModel');
+const SiteDocument= require('../../models/site/siteDocumentModel');
 const Site = require('../../models/site/siteModel');
-
 class SiteDocumentController {
-  //add note
-  static async addSiteDoc(req, res) {
-    try {
-      const { siteId } = req.params;
-      const { documentName, requireAllStaff, requiresAcknowledgementBeforeShift, requireSignature, needRenenwal, renenwalPeriodMonth } = req.body;
-      let attachFile = [];
-
-      if (req.file) {
-        attachFile = [{
-          fileName: req.file.originalname,
-          fileUrl: `/uploads/site-notes/${req.file.filename}`,
-          site: siteId,
-        }];
-      }
-
-      const newSiteDocument = await SiteDocument.create({
-        documentName,
-        requireAllStaff,
-        requiresAcknowledgementBeforeShift,
-        requireSignature,
-        needRenenwal,
-        renenwalPeriodMonth,
+    //add note
+    static async addSiteDoc(req,res){
+        try{
+            const{siteId} = req.params;
+            const{documentName,requireAllStaff,requiresAcknowledgementBeforeShift,requireSignature,needRenenwal,renenwalPeriodMonth}= req.body;
+            let attachFile = [];
+            
+    if (req.file) {
+      attachFile = [{
+        fileName: req.file.originalname,
+        fileUrl: `/uploads/site-notes/${req.file.filename}`,
+        site: siteId, 
+      }];
+    }
+    const newSiteDocument = await SiteDocument.create({
+       documentName,
+       requireAllStaff,
+       requiresAcknowledgementBeforeShift,
+       requireSignature,needRenenwal,
+       renenwalPeriodMonth,
         attachFile,
-        siteId: siteId
-      })
-
-      await Site.findByIdAndUpdate(siteId, {
-        $push: { siteDocuments: newSiteDocument._id }
-      })
-
-      return res.status(201).json({
-        message: "Site document is created successfully",
-        data: newSiteDocument,
-      })
-    } catch (error) {
+        site: siteId
+    })
+    
+    await Site.findByIdAndUpdate(siteId, {
+        $push: { notes: newSiteDocument._id }
+    })
+    return res.status(201).json({
+        message:"Site document is created successfully",
+        data:newSiteDocument,
+    })
+        }
+        catch (error) {
       res.status(500).json({ message: error.message });
     }
   }
-
-  // get notes 
-  static async getSiteDoc(req, res) {
+  //get notes 
+  static async getSiteDoc(req, res){
     try {
-      const documents = await SiteDocument.find().populate("siteId");
-
+      const notes = await SiteDocument.find().populate("site");
+  
       res.status(200).json({
-        message: "Documents fetched successfully",
-        data: documents
+        message: "Notes fetched successfully",
+        data: notes
       });
-
+  
     } catch (error) {
       res.status(500).json({
         message: error.message
@@ -60,12 +56,12 @@ class SiteDocumentController {
   //update notes
   static async updateSiteDoc(req, res) {
       try {
-        const updateDocuments = await SiteDocument.findByIdAndUpdate(
+        const updateNotes = await SiteDocument.findByIdAndUpdate(
           req.params.id,
           req.body,
           { new: true }
         )
-        res.json(updateDocuments);
+        res.json(updateNotes);
       } catch (err) {
         res.status(500).json({ message: err.message });
       }
